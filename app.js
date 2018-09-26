3c66ca2069011f5440547f99db371b81ff556b0b
neogeoss <neogeo-s@hanmail.net>
2018-09-26 04:30:59 +0000

    authentication practice

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(require("express-session")({
     secret: "Rusty is the best and cutest dog in the world",
     resave: false,
@@ -20,6 +21,7 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -27,9 +29,52 @@ app.get('/', function(req, res){
     res.render('home');
 });
 
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
    res.render("secret"); 
 });
+
+app.post('/register', function(req, res){
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        });
+    });
+});
+
+app.get('/register', function(req,res){
+    res.render('register');
+});
+
+app.get("/login", function(req,res){
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local",{
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req,res){
+    
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+    
+    
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server started!");
